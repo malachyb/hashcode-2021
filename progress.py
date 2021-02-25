cca7861d2e09517c66030e60c240884d4be06f67
malmal200 <malachybyrne1@gmail.com>
2021-02-25 19:51:34 +0000

    created progress.py

---
+++ progress.py
@@ -0,0 +1,12 @@
+def progress(intersections, all_pos):
+    reds = []
+    for i, pos in enumerate(all_pos):
+        if intersections[pos[1]] != pos[0]:
+            reds.append(i)
+    for i, row in enumerate(all_pos):
+        if i == 0:
+            continue
+        for red in reds:
+            row[i][red] = row[i-1][red]
+    all_pos.pop(0)
+    return all_pos
