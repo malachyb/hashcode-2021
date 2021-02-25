b3be8c1303b3107c789953bb3b834bd68730e1d7
malmal200 <malachybyrne1@gmail.com>
2021-02-25 18:54:23 +0000

    streets now keep track of intersections

--- street_input.py
+++ street_input.py
@@ -1,15 +1,15 @@
 def street_input(filename, intersections, streets, cars):
     list_intersections = [[] for _ in range(intersections)]
     dict_streets = {}
-    with open (filename, "r") as fd:
+    with open(filename, "r") as fd:
         lines = fd.readlines()
         for i in range(1, streets):
             line = lines[i].strip().split()
 
-            list_intersections[line[0]].append(line[2])
-            list_intersections[line[1]].append(line[2])
+            list_intersections[int(line[0])].append(line[2])
+            list_intersections[int(line[1])].append(line[2])
 
-            dict_streets[line[2]] = {"Time" : line[3]}
+            dict_streets[line[2]] = {"time": line[3], "intersections": line[0:2]}
         list_cars = []
         for i in range(cars):
             line = lines[i].strip().split()
